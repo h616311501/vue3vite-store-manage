1bdf7db181ce2c00bce950a5f6bcff6ce2337c7f
Wits <39427850+h616311501@users.noreply.github.com>
2026-01-12 10:02:51 +0800

    feat: 基本框架

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    WindiCSS(),
+  ],
+  server: {
+    host: true,
+    port: 3210,
+  },
+})
